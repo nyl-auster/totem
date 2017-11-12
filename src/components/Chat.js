75ad5e545b560e012572d38090f256cfc27952a7
yann <yann.boisselier@gmail.com>
2017-11-12 11:50:35 +0100

    try another way to start node

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -12,7 +12,7 @@ class Chat extends Component {
     };
 
     // connect to our chat server
-    this.socket = io("https://localhost:8000", {
+    this.socket = io("http://localhost:8000", {
       query: `username=${props.userName}`
     }).connect();
 
